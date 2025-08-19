93471a3dec120f5ec80e3c63e50409937dec405d
Adam Hoban <S00253815@atu.ie>
2025-08-19 09:41:47 +0100

    added class file and enum

---
+++ AdamhobanOOPAuthumExam/BikeClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdamhobanOOPAuthumExam
+{
+    public enum BikeType
+    {
+        Petrol,
+        Diesel,
+        Electric,
+        Human
+    }
+    internal class BikeClass
+    {
+    }
+}
