1a36a818bb5043ea9ac6fe2a504c73817d11e7aa
Adam Hoban <S00253815@atu.ie>
2025-08-19 09:48:08 +0100

    made bike class and added methods

--- AdamhobanOOPAuthumExam/BikeClass.cs
+++ AdamhobanOOPAuthumExam/BikeClass.cs
@@ -13,9 +13,31 @@ namespace AdamhobanOOPAuthumExam
         Electric,
         Human
     }
-    internal class Engine
+    public class Engine
     {
         public BikeType Fuel { get; set; }
         public int HorsePower { get; set; }
+    }
+    public abstract class Bike
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public DateTime DateOfManufacture { get; set; }
+        public Engine Engine { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Make} {Model} ({DateOfManufacture.Year}) - {Engine.Fuel} with {Engine.HorsePower} HP";
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Make: {Make}");
+            Console.WriteLine($"Model: {Model}");
+            Console.WriteLine($"Year: {DateOfManufacture.ToShortDateString()}");
+            Console.WriteLine($"fuel: {Engine.Fuel}");
+            Console.WriteLine($"HorsePower: {Engine.HorsePower}");
+        }
+
     }
 }
